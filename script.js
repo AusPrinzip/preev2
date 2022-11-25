928119a6780eaf6bf8d58459e08f13404f5ce3cf
AusPrinzip <cdmdb@hotmail.com>
2022-11-25 12:45:18 -0600

    Reworked input fields mechanics

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 
-$('.css-v8fmq5').css("width", "350px");
-// css-1p8s75i
+// Trick to get cursor to focus on the right most side of the input field
+$('#btn-input').focus();
+$('#btn-input').val(0);
+$('#btn-input').val(1);
+
+
 $('#btn-input').on("input", function() {
    const newVal = $(this).val();
    const newValLength = newVal.length
-   $('.css-1p8s75i').css("width", `${50 + newValLength * 30}px`)
+   $('.css-1p8s75i').css("width", `${50 + newValLength * 25}px`)
 });
 
 $('#btn-input2').on("input", function() {
    const newVal = $(this).val();
    const newValLength = newVal.length
-   $('.css-v8fmq5').css("width", `${50 + newValLength * 30}px`)
+   $('.css-v8fmq5').css("width", `${50 + newValLength * 25}px`)
 });
 
 $( document ).ready(
-
 	$.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').done((res) => {
 		let currentPrice = res[0].current_price
 		$('#btn-input2').val(currentPrice)
-		console.log(currentPrice)
+		const newValLength = currentPrice.toString().length;
+		$('.css-v8fmq5').css("width", `${50 + newValLength * 25}px`)
 	})
+
 )
