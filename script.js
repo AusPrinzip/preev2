dc824b5b76da9b69aaf4747abc7e60c1049e7cd9
AusPrinzip <cdmdb@hotmail.com>
2022-11-25 12:26:07 -0600

    Applied same logic from last commit to second input field

--- script.js
+++ script.js
@@ -7,8 +7,14 @@ $('#btn-input').on("input", function() {
    $('.css-1p8s75i').css("width", `${50 + newValLength * 30}px`)
 });
 
+$('#btn-input2').on("input", function() {
+   const newVal = $(this).val();
+   const newValLength = newVal.length
+   $('.css-v8fmq5').css("width", `${50 + newValLength * 30}px`)
+});
+
 $( document ).ready(
-	
+
 	$.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').done((res) => {
 		let currentPrice = res[0].current_price
 		$('#btn-input2').val(currentPrice)
