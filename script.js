94e4b3fcd20fc3ef4ec704a764954b3f853e0cf2
AusPrinzip <cdmdb@hotmail.com>
2022-11-25 13:19:57 -0600

    Prevented field input change from creating an infinite loop of retrofeed: each input change should trigger a single update on the other one. Input#2 should have no decimals. Input#1 should have 3 decimals

--- script.js
+++ script.js
@@ -8,26 +8,29 @@ var currentPrice = 0;
 
 // Automatically change inputfield #1 width based on value length AND Value update whenever inputfield#1 changes
 $('#btn-input').on("input", function() {
-   const newVal = $(this).val();
-   const newValLength = newVal.length
-   $('.css-1p8s75i').css("width", `${50 + newValLength * 25}px`)
-   $('#btn-input2').val(newVal * currentPrice)
+   const newVal1 = $(this).val();
+   $('.css-1p8s75i').css("width", `${50 + newVal1.toString().length * 35}px`)
+   const newVal2 = parseFloat(newVal * currentPrice).toFixed(3)
+   $('#btn-input2').val(newVal2)
+   $('.css-v8fmq5').css("width", `${50 + newVal2.toString().length * 35}px`)
 });
 
 // Automatically change inputfield #2 width based on value length
 $('#btn-input2').on("input", function() {
-   const newVal = $(this).val();
-   const newValLength = newVal.length
-   $('.css-v8fmq5').css("width", `${50 + newValLength * 25}px`)
+   const newVal1 = $(this).val()
+   $('.css-v8fmq5').css("width", `${50 + newVal1.toString().length * 35}px`)
+   const newVal2 = parseFloat(currentPrice / newVal1).toFixed(3)
+   $('#btn-input').val(newVal2)
+   $('.css-1p8s75i').css("width", `${50 + newVal2.toString().length * 35}px`)
 });
 
 // BTC price initial update and inputfield#2 width update
 $( document ).ready(
 	$.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').done((res) => {
 		currentPrice = res[0].current_price
-		$('#btn-input2').val(currentPrice)
+		$('#btn-input2').val(Math.round(currentPrice))
 		const newValLength = currentPrice.toString().length;
-		$('.css-v8fmq5').css("width", `${50 + newValLength * 25}px`)
+		$('.css-v8fmq5').css("width", `${50 + newValLength * 35}px`)
 	})
 )
 
